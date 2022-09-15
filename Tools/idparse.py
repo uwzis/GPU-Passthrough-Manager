f7a5c573113e2a94482e8ad82aeba89b926c420f
mpxf <diamond061@outlook.com>
2022-09-15 11:51:15 -0400

    Wrote idparse.py

--- Tools/idparse.py
+++ Tools/idparse.py
@@ -1,1 +1,21 @@
+# Import dependencies
+import sys
+import re
 
+def main(_fp):   
+    # Initialize output variable
+    _o = []
+
+    # Open IDs list file
+    _fs = open(_fp, 'r')
+    _l = _fs.readlines()
+    for l in _l:
+        _o.append(re.search(r"[0-9,a-f]{4}:[0-9,a-f]{4}", l).group())
+
+    # Output
+    print(','.join(_o))
+
+# Only run directly
+if __name__ == "__main__":
+    main(sys.argv[1])
+    sys.exit(0)
