dbf72d67b2dc7fbe5744e19deead89d6e9cd5b26
mpxf <diamond061@outlook.com>
2022-09-15 22:27:03 -0400

    Added line number argument to idparse.py

--- Tools/idparse.py
+++ Tools/idparse.py
@@ -2,20 +2,27 @@
 import sys
 import re
 
-def main(_fp):   
+# Main function
+def main(_fp, _ln = None):   
     # Initialize output variable
     _o = []
 
     # Open IDs list file
     _fs = open(_fp, 'r')
     _l = _fs.readlines()
-    for l in _l:
-        _o.append(re.search(r"[0-9,a-f]{4}:[0-9,a-f]{4}", l).group())
+
+    # Separate parsing methods
+    if not _ln:
+        # Iterate through line
+        for l in _l:
+            _o.append(re.search(r"[0-9,a-f]{4}:[0-9,a-f]{4}", l).group())
+    else:
+        # Get ID from line number
+        _o.append(re.search(r"[0-9,a-f]{4}:[0-9,a-f]{4}", _l[int(_ln)-1]).group())
 
     # Output
-    print(','.join(_o))
+    return ','.join(_o)
 
-# Only run directly
+# Only run as script
 if __name__ == "__main__":
-    main(sys.argv[1])
-    sys.exit(0)
+    print(main(*sys.argv[1:]))
