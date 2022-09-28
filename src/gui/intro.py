44beb3eb23c256def83469881dfaaf81c592658f
mpxf <diamond061@outlook.com>
2022-09-28 13:56:40 -0400

    Added warning to not be responsible for damage

--- src/gui/intro.py
+++ src/gui/intro.py
@@ -24,9 +24,14 @@ class window(QWidget):
         self.firstMessageTitle.setFont(QFont('default', 15))
         
         # Body
-        self.firstMessageBody = QLabel("This program is used for assisting in passing through graphics/audio devices to virtual machines. To begin, click the button below to run initial compatibility checks and configuration adjustments.")
+        self.firstMessageBody = QLabel("This program is used for assisting in passing through graphics/audio devices to virtual machines. To begin, click the button below to run initial compatibility checks and configuration adjustments. Once it finishes, your system will automatically reboot to apply the changes.")
         self.firstMessageBody.setFont(QFont("default", 11))
         self.firstMessageBody.setWordWrap(True)
+
+        # Warning
+        self.firstMessageWarning = QLabel("Before continuing, ensure your system supports IOMMU and virtualization is enabled in BIOS. Installation will fail if these requirements are not met, and your operating system may become unstable and/or unusable.")
+        self.firstMessageWarning.setFont(QFont("default", 11, weight=QFont.Bold))
+        self.firstMessageWarning.setWordWrap(True)
 
         # Button
         self.firstMessageButton = QPushButton("Start")
@@ -36,11 +41,12 @@ class window(QWidget):
         # Add GUI elements to layout
         self.layout.addWidget(self.firstMessageTitle, 0, 0)
         self.layout.addWidget(self.firstMessageBody, 1, 0)
-        self.layout.addWidget(self.firstMessageButton, 2, 0)
+        self.layout.addWidget(self.firstMessageWarning, 2, 0)
+        self.layout.addWidget(self.firstMessageButton, 3, 0)
 
         # Display window
         self.setLayout(self.layout)
-        self.setFixedSize(QSize(400, 175))
+        self.setFixedSize(QSize(400, 300))
         self.show()
 
     # Button event handler
