7e902b8d9ea3a4eec2b615523666c945f7c0cba5
mpxf <diamond061@outlook.com>
2022-09-28 10:09:20 -0400

    Update GUI design

--- GPUPassthroughManager.py
+++ GPUPassthroughManager.py
@@ -1,5 +1,5 @@
 # Import dependencies
-import sys
+import sys, os
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -17,24 +17,34 @@ class window(QWidget):
         self.layout = QGridLayout()
 
         # Initialize GUI elements
+        # Title
         self.firstMessageTitle = QLabel("Welcome to GPU Passthrough Manager!", self)
-        self.firstMessageTitle.setFont(QFont('default', 20))
+        self.firstMessageTitle.setFont(QFont('default', 15))
         
+        # Body
         self.firstMessageBody = QLabel("This program is used for assisting in passing through graphics/audio devices to virtual machines. To begin, click the button below to run initial compatibility checks and configuration adjustments.")
         self.firstMessageBody.setFont(QFont("default", 11))
         self.firstMessageBody.setWordWrap(True)
 
+        # Button
+        self.firstMessageButton = QPushButton("Start")
+        self.firstMessageButton.setFont(QFont("default", 12))
+
         # Add GUI elements to layout
         self.layout.addWidget(self.firstMessageTitle, 0, 0)
         self.layout.addWidget(self.firstMessageBody, 1, 0)
+        self.layout.addWidget(self.firstMessageButton, 2, 0)
 
         # Display window
         self.setLayout(self.layout)
-        self.setFixedWidth(self.width())
+        self.setFixedSize(QSize(400, 175))
         self.show()
 
 # Prevent running from imports
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = window()
-    sys.exit(app.exec_())
+    if os.geteuid() == 0:
+        app = QApplication(sys.argv)
+        win = window()
+        sys.exit(app.exec_())
+    else:
+        sys.exit("You must run GPU Passthrough Manager as root.")
