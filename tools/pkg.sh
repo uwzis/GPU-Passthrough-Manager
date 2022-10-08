9678650e2889a3d1faa7541a199f9063699f0e80
uwzis <102548317+uwzis@users.noreply.github.com>
2022-10-08 18:51:32 -0400

    Update pkg.sh

--- tools/pkg.sh
+++ tools/pkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 echo "Setting up GPUPM"
-sudo mv ./gpu-passthrough-manager /usr/share/
+sudo mv ./gpu-passthrough-manager /usr/share/gpu-passthrough-manager
 sudo mv /usr/share/gpu-passthrough-manager/gpu-passthrough-manager /usr/bin/
 sudo mv /usr/share/gpu-passthrough-manager/icons/GPU\ Passthrough\ Manager.desktop /usr/share/applications/
 echo "SETUP IS COMPLETE!"
