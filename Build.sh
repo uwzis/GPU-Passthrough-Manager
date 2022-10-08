1e8eca4367c7618bcfc71212f920c48f3c06bf22
uwzis <102548317+uwzis@users.noreply.github.com>
2022-10-08 12:38:25 -0400

    Add files via upload

--- Build.sh
+++ Build.sh
@@ -2,3 +2,4 @@
 g++ ./GPUPM.cpp -o "GPUPM" `pkg-config --cflags --libs jsoncpp  gtk+-3.0`
 gcc ./tools/reboot.c -o "./tools/Reboot" `pkg-config --cflags --libs gtk+-3.0`
 gcc ./tools/apphandler.c -o "gpu-passthrough-manager"
+mv ./gpu-passthrough-manager /usr/bin/
