3ddb5e990ef926390d6b7782b1cba1b88ca67b9c
uwzis <102548317+uwzis@users.noreply.github.com>
2022-10-09 13:17:12 -0400

    Update pkg.sh

--- tools/pkg.sh
+++ tools/pkg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Setting up GPUPM"
-sudo mkdir -p /usr/share/gpu-passthrough-manager
 sudo install -Dm775 ./gpu-passthrough-manager /usr/bin/gpu-passthrough-manager
+sudo install -Dm775 ./README.md /usr/share/doc/gpu-passthrough-manager/README.md
+sudo mkdir -p /usr/share/gpu-passthrough-manager
 sudo install -Dm775 ./icons/GPU\ Passthrough\ Manager.desktop /usr/share/applications/GPU\ Passthrough\ Manager.desktop
 sudo install -Dm775 ./GPUPM /usr/share/gpu-passthrough-manager/GPUPM
 sudo install -Dm775 ./style.css /usr/share/gpu-passthrough-manager/style.css
-sudo install -Dm775 ./README.md /usr/share/doc/gpu-passthrough-manager/README.md
 sudo mkdir -p /usr/share/gpu-passthrough-manager/tools
 sudo install -Dm775 ./tools/Reboot /usr/share/gpu-passthrough-manager/tools/Reboot
 sudo install -Dm775 ./tools/defaultmgr.sh /usr/share/gpu-passthrough-manager/tools/defaultmgr.sh 
