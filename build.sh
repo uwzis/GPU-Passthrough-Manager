f30553068cff6779ac26c2af3b3ad51935bf5b71
Sean "Jesus" Cohmer <93715513+aprunge@users.noreply.github.com>
2022-11-02 14:06:30 -0400

    fixed your lil retard freakout

--- build.sh
+++ build.sh
@@ -2,7 +2,3 @@
 g++ ./GPUPM.cpp -o "GPUPM" `pkg-config --cflags --libs jsoncpp  gtk+-3.0`
 gcc ./tools/reboot.c -o "./tools/Reboot" `pkg-config --cflags --libs gtk+-3.0`
 gcc ./tools/apphandler.c -o "gpu-passthrough-manager"
-rm ./GPUPM.cpp
-rm ./tools/reboot.c
-rm ./tools/apphandler.c
-rm ./Build.sh
