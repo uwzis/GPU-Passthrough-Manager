f4f17b3a3ee611088f80057bcf52824f7acc41b1
uwzis <102548317+uwzis@users.noreply.github.com>
2022-11-23 18:01:48 -0500

    adds initramfs support pt2

--- tools/vfiomgr.sh
+++ tools/vfiomgr.sh
@@ -2,4 +2,11 @@
 echo "Copying configuration file..."
 cp ./vfio.conf /etc/modprobe.d/
 echo "Rebuilding system images..."
-mkinitcpio -P linux
+INITRAM=/etc/initramfs-tools/modules
+if [ -f "$INITRAM" ]; then
+    update-initramfs -u
+fi
+MKINIT=/etc/mkinitcpio.conf
+if [ -f "$MKINIT" ]; then
+	mkinitcpio -P linux
+fi
