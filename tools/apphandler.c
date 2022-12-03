43e5698d49f0e69916a5f819b59d15c357212206
uwzis <102548317+uwzis@users.noreply.github.com>
2022-12-02 23:31:25 -0500

    Update apphandler.c

--- tools/apphandler.c
+++ tools/apphandler.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int main () {
-    system("cd /usr/share/gpu-passthrough-manager/ && pkexec ./GPUPM");
+    
+    system("cd /usr/share/gpu-passthrough-manager/ && ./GPUPM");
     return 0;
 }
